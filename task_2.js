96cdf0497e179af38cf083b25ceb050eb6c28319
Айсулу <aisuluueco2009@yandex.ru>
2019-11-25 16:24:28 +0600

    Внесла изменение.

--- task_2.js
+++ task_2.js
@@ -1,8 +1,7 @@
 let element = document.querySelectorAll(".element");
 let i;
-let red = 3;
 for (i = 0; i < element.length; i++){
-	if (i < red) {
+	if (i < 3) {
 	element[i].style.backgroundColor = "red";
 	}else {
 	element[i].style.backgroundColor = "green";
